b1d7f6fd795b0ed8bd4ce80cfc7536d3f5fe35f4
ryan180322 <chungen.ouyang@students.mq.edu.au>
2025-05-12 22:03:35 +1000

    more tiled

--- Assets/Tiled/MyTilemapGame/Tilesheet.tsx
+++ Assets/Tiled/MyTilemapGame/Tilesheet.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeon_Tilesheet" tilewidth="16" tileheight="16" tilecount="132" columns="12">
- <image source="../../../../../../Downloads/kenney_tiny-dungeon/Tilemap/tilemap_packed.png" width="192" height="176"/>
+<tileset version="1.10" tiledversion="1.11.2" name="Tilesheet" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="tilemap_packed.png" width="192" height="176"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
